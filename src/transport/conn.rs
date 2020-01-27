dd0c014a33d2cfacfdf8d29eb1287b4ed960d45f
avitex <theavitex@gmail.com>
2020-01-27 21:45:43 +1100

    start impl async read

--- src/transport/conn.rs
+++ src/transport/conn.rs
@@ -1,4 +1,10 @@
 use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::Duration;
+
+use futures_timer::Delay;
+use futures::io::{self, AsyncRead, AsyncWrite};
 
 use crate::packet::Packet;
 use crate::transport::Datagram;
@@ -16,13 +22,32 @@ where
 
 pub struct Connection<T> {
     transport: T,
+    timeout_waker: Delay,
+    timeout_waiting: bool,
 }
 
 impl<'a, T> Connection<T>
 where
     T: ExchangeTransport<'a, Packet<'a>>,
 {
-    pub fn new(transport: T) -> Self {
-        Self { transport }
+    pub fn new(transport: T, poll_interval: Duration) -> Self {
+        Self {
+            transport,
+            timeout_waker: Delay::new(poll_interval),
+            timeout_waiting: false,
+        }
     }
 }
+
+// impl<'a, T> AsyncRead for Connection<T>
+// where
+//     T: ExchangeTransport<'a, Packet<'a>>,
+// {
+//     fn poll_read(
+//         self: Pin<&mut Self>,
+//         cx: &mut Context,
+//         buf: &mut [u8],
+//     ) -> Poll<Result<usize, io::Error>> {
+
+//     }
+// }
--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -3,7 +3,5 @@ mod datagram;
 
 pub mod dns;
 
-pub use futures::io::{AsyncRead, AsyncWrite};
-
 pub use self::conn::*;
 pub use self::datagram::*;
